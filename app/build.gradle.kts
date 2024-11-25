64c892d6d8dbcbdf8f86d0e5436c15273dc12d0f
GilangSN23 <119424777+GilangSN23@users.noreply.github.com>
2024-11-25 15:56:11 +0700

    update ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,9 @@ android {
 }
 
 dependencies {
-
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("org.jetbrains.kotlin:kotlin-script-runtime")
+    implementation("org.jetbrains.kotlin:kotlin-scripting-compiler-embeddable")
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")

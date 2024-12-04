d3601bb99c18d12d81518d04383baf081c8e3e3f
GilangSN23 <119424777+GilangSN23@users.noreply.github.com>
2024-12-04 17:25:34 +0700

    navigasi sementara

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    id("com.google.devtools.ksp")
 }
 
 android {
     namespace = "com.capstone.free.education"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.capstone.free.education"
@@ -43,14 +44,16 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.media3:media3-common-ktx:1.5.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
     implementation("androidx.activity:activity-ktx:1.7.2")
     implementation ("androidx.navigation:navigation-fragment-ktx:2.7.1")
     implementation ("androidx.navigation:navigation-ui-ktx:2.7.1")
+    implementation ("com.google.android.material:material:1.9.0")
+
 }
--- app/src/main/java/com/capstone/free/education/view/main/MainActivity.kt
+++ app/src/main/java/com/capstone/free/education/view/main/MainActivity.kt
@@ -3,13 +3,22 @@ package com.capstone.free.education.view.main
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowInsets
 import android.view.WindowManager
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.capstone.free.education.R
 import com.capstone.free.education.databinding.ActivityMainBinding
 import com.capstone.free.education.view.ViewModelFactory
 import com.capstone.free.education.view.welcome.WelcomeActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private val viewModel by viewModels<MainViewModel> {
@@ -21,6 +30,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+
+        if (navHostFragment is NavHostFragment) {
+            val navController = navHostFragment.navController
+
+            // Menyambungkan BottomNavigationView dengan NavController
+            val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+            NavigationUI.setupWithNavController(bottomNavigationView, navController)
+
+            // Handle pergerakan fragment berdasarkan pilihan bottom navigation
+            bottomNavigationView.setOnNavigationItemSelectedListener {
+                when (it.itemId) {
+                    R.id.nav_home -> {
+                        navController.navigate(R.id.home_nav)
+                        true
+                    }
+                    R.id.nav_setting -> {
+                        navController.navigate(R.id.setting_nav)
+                        true
+                    }
+                    R.id.nav_profile -> {
+                        navController.navigate(R.id.profile_nav)
+                        true
+                    }
+                    else -> false
+                }
+            }
+        } else {
+            Log.e("MainActivity", "NavHostFragment tidak ditemukan atau cast gagal")
+        }
+
+
 
         viewModel.getSession().observe(this) { user ->
             if (!user.isLogin) {

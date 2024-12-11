240ec0b002d8302e0b46961b9661e1bfd9f13eaf
MCHAN7 <mchandra7733@gmail.com>
2024-12-11 15:22:46 +0700

    update

--- app/src/main/java/com/capstone/free/education/view/home/HomeFragment.kt
+++ app/src/main/java/com/capstone/free/education/view/home/HomeFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.capstone.free.education.R
-import com.capstone.free.education.databinding.FragmentHomeBinding
 import com.capstone.free.education.view.materi.MateriFragment
 import com.capstone.free.education.view.konsultasi.KonsultasiFragment
 import com.capstone.free.education.view.reportlink.ReportFragment
 import com.capstone.free.education.view.selfcheck.selfcheckFragment
+import com.capstone.free.education.view.setting.SettingFragment
 import com.capstone.free.education.view.spendingscore.spendingscoreFragment
 import com.dicoding.mysubmission.ui.home.EventHomeFragment
 
@@ -27,29 +27,31 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Initialize the buttons and set click listeners
-        view.findViewById<View>(R.id.fab1).setOnClickListener {
-            navigateToFragment(EventHomeFragment())
+        // Menghubungkan CardView dari XML ke fragment terkait
+        view.findViewById<View>(R.id.card_setting).setOnClickListener {
+            navigateToFragment(SettingFragment())
         }
-        view.findViewById<View>(R.id.fab2).setOnClickListener {
+        view.findViewById<View>(R.id.card_self_check).setOnClickListener {
             navigateToFragment(selfcheckFragment())
         }
-        view.findViewById<View>(R.id.fab3).setOnClickListener {
+        view.findViewById<View>(R.id.card_spending_score).setOnClickListener {
             navigateToFragment(spendingscoreFragment())
         }
-        view.findViewById<View>(R.id.fab4).setOnClickListener {
+        view.findViewById<View>(R.id.card_report).setOnClickListener {
             navigateToFragment(ReportFragment())
         }
-        view.findViewById<View>(R.id.fab5).setOnClickListener {
+        view.findViewById<View>(R.id.card_consultation).setOnClickListener {
             navigateToFragment(KonsultasiFragment())
         }
-        // fb6 doesn't need a click listener as it's not used.
+        view.findViewById<View>(R.id.card_material).setOnClickListener {
+            navigateToFragment(EventHomeFragment())
+        }
     }
 
     private fun navigateToFragment(fragment: Fragment) {
         parentFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, fragment) // Replace with your container ID
-            .addToBackStack(null) // Add to back stack to allow back navigation
+            .replace(R.id.fragment_container, fragment) // Ganti dengan ID container di layout utama
+            .addToBackStack(null) // Tambahkan ke back stack untuk navigasi kembali
             .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
             .commit()
     }
